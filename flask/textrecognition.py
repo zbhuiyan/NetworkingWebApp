71de44a507829b8e4758db70d32d6e42eb95e920
Zarin Bhuiyan <zarin.bhuiyan@students.olin.edu>
2015-03-28 12:57:07 -0400

    trying to add csv thing

--- flask/textrecognition.py
+++ flask/textrecognition.py
@@ -7,6 +7,7 @@
 #Imports:
 import numpy as np
 import cv2
+from cv2 import cv
 from PIL import Image
 import pytesseract
 import csv
@@ -48,22 +49,26 @@ def filter_image(imgFile):
     #writing to file
     cv2.imwrite('image.jpg', img)
 
-def subimage(imgFile):  
-
-    centre = (110,125)
-    theta = np.pi/6.0
+def subimage(imgFile, angle):  
+    # img = cv2.imread(imgFile,0)
+    img = cv.LoadImage(imgFile)
+    imgcv2 = cv2.imread('image.jpg',0)
+    width = imgcv2.shape[1]
+    height = imgcv2.shape[0]
+    centre = (width/2, height/2)
+    theta = angle #in radians
     width = 100
     height = 200
-
-    # patch = subimage(imgFile, (110, 125), np.pi / 6.0, 100, 200)
+    
     # cv.SaveImage('image.jpg', patch)
 
-    imgFile = cv.CreateImage((width, height), image.depth, image.nChannels)
+    imgFile = cv.CreateImage((width, height), img.depth, img.nChannels)
     mapping = np.array([[np.cos(theta), -np.sin(theta), centre[0]],
                         [np.sin(theta), np.cos(theta), centre[1]]])
     map_matrix_cv = cv.fromarray(mapping)
-    cv.GetQuadrangleSubPix(image, img, map_matrix_cv
-    return imgFile
+    cv.GetQuadrangleSubPix(img, img, map_matrix_cv)
+    cv.SaveImage('rotation_test.jpg', img)
+    return img
 
 
 def extract_text(imgFile):
@@ -77,6 +82,27 @@ def write_text(text, outFile):
         output = csv.writer(csvfile, delimiter= ' ', quotechar = '|', quoting = csv.QUOTE_MINIMAL)
         output.writerow([text])
 
+
+def csv_write(imgFile):
+    imgFile_list = []
+    writing = csv.writer(open('output.csv', 'wb'), delimiter= ',')
+    writing.writerows(imgFile_list)
+
+
+
+
+
+#     example=csv.writer(open('test.csv', 'wb'), delimiter=' ')
+# example.writerows( [[1, 2], [2, 3], [4, 5]])
+
+#     out = open('out.csv', 'w')
+# for row in l:
+#     for column in row:
+#         out.write('%d;' % column)
+#     out.write('\n')
+# out.close() 
+
+
 def main():
     """Main sequence for gathering image, and outputting text to file"""
     #file names
@@ -86,7 +112,7 @@ def main():
     #main sequence
 
     get_image(imgFile)
-    subimage(imgFile)
+    subimage(imgFile, np.pi/ 6.0)
     filter_image(imgFile)
     extractedText = extract_text(imgFile)
     write_text(extractedText, outFile)
